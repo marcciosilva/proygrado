07d1a95a772e597ab489ccc36fe094fce34f9045
Marccio Silva <marccio.silva@fing.edu.uy>
2017-08-26 19:20:26 -0300

    Se agrega opción al generar_jobs.py para que un job se encargue únicamente de generar los datos crudos, y después los demás jobs sí sean paralelizables. Esto se controla con la variable SERIAL_INITIAL_DATA_GENERATION.

--- generador-instancias/generar_jobs.py
+++ generador-instancias/generar_jobs.py
@@ -4,6 +4,7 @@ Este script genera un (o varios) .sh que se va a encargar de generar ejemplos de
 '''
 import os
 import errno
+import math
 
 PARENT_DIR = ''
 # Donde van a quedar alojados los scripts finales
@@ -16,11 +17,6 @@ GENERATOR_DIR = 'ejemplos-entrenamiento-separados/'
 PARSER = 'python parser.py '
 # Directorio donde se van a dejar los ejemplos unificados
 PARSER_DIR = 'ejemplos-entrenamiento-unificados/'
-# En este array se van a guardar los comandos a ejecutar para generar instancias
-# Se van a tener tantos elementos como archivos de generacion existan
-# La idea es generar tantos archivos como jobs se quieran ejecutar en el cluster,
-# para paralelizar
-commands = []
 # Cantidad de jobs a iniciar para generar ejemplos de entrenamiento
 # Deberia ser un numero par (para que en el split de listas no queden separados
 # la generacion y el parsing de un ejemplo)
@@ -31,65 +27,120 @@ TEST_TRAINING_AMOUNT = [100, 600]
 # Tipos de problema, donde la clave indica la cantidad de tareas del problema, y el valor
 # indica la cantidad de maquinas
 PROBLEM_TYPES = {128: 4, 512: 16}
+# Whether to generate data serially (so one job will basically generate all of the .in and .out pairs for
+# all types of problems FIRST, while the rest can be executed in parallel later) or not.
+SERIAL_INITIAL_DATA_GENERATION = True
 
 def chunks(lst, chunk_amount):
     """
-    Dada una lista lst y un parametro chunk_amount, se devuelven listas de n elementos en la medida
-    de lo posible
+    Splits list lst in lists of chunk_amount elements and returns them (as a list of lists)
     """
     chunk_amount = max(1, chunk_amount)
     return [lst[i:i + chunk_amount] for i in xrange(0, len(lst), chunk_amount)]
 
 def generate_dir(path):
     """
-    Si el directorio dado por path no existe, lo crea
+    If path doesn't exist, it gets created
     """
     try:
         os.makedirs(path)
-        print 'Directory ' + path + ' created.'
+        print 'Directory ' + path + ' created or already existed.'
     except OSError as error:
         if error.errno != errno.EEXIST:
             raise
 
 def main():
+    # En este array se van a guardar los comandos a ejecutar para generar instancias
+    # Se van a tener tantos elementos como archivos de generacion existan
+    # La idea es generar tantos archivos como jobs se quieran ejecutar en el cluster,
+    # para paralelizar
+    commands = []    
+    # Generate jobs directory
+    generate_dir(OUTPUT_PATH)
+    if SERIAL_INITIAL_DATA_GENERATION:
+        print 'Working with serial raw data generation'
+        # Create serial job that generates training examples (doesn't handle parsing)
+        for machine_amount in range(0, 2):
+            for task_heterogeneity_type in range(0, 2):
+                for machine_heterogeneity_type in range(0, 3):
+                    # For each problem type or dimension
+                    for tasks in PROBLEM_TYPES.keys():
+                        for instance_amount in TEST_TRAINING_AMOUNT:
+                            machines = PROBLEM_TYPES[tasks]
+                            # If I'm generating training examples, the path suffix is changed
+                            if instance_amount == TEST_TRAINING_AMOUNT[0]:
+                                dir_suffix = '/test/'
+                            else:
+                                dir_suffix = '/training/'
+                            # sub_dir is the identifier for each problem instance
+                            sub_dir = str(tasks) + 'x' + str(machines) + '-' + \
+                                str(machine_amount) + str(task_heterogeneity_type) + \
+                                str(machine_heterogeneity_type) + dir_suffix
+                            directory = GENERATOR_DIR + sub_dir
+                            # Add commands to commands object
+                            commands.append(GENERATOR + str(tasks) + ' ' + str(machines) + ' ' \
+                                + str(machine_amount) + ' ' + str(task_heterogeneity_type) + ' ' \
+                                + str(machine_heterogeneity_type) + ' ' + str(instance_amount) \
+                                + ' ' + directory)
+                            # Output directory is generated for later use
+                            generate_dir(directory)
+        # Data generation job is created
+        # This job should always be executed first
+        tmp_file = open(OUTPUT_PATH + 'job-generate-raw-data' + '.sh', 'w')
+        for command in commands:
+            tmp_file.write(command + '\n')
+        tmp_file.close()
+        print 'Raw data generation script generated in ' + OUTPUT_PATH
+        # Clean up
+        commands = []
+    ###########################################################################################
+    ###########################################################################################
+
     for machine_amount in range(0, 2):
         for task_heterogeneity_type in range(0, 2):
             for machine_heterogeneity_type in range(0, 3):
-                # Para cada tipo o dimension del problema
+                # For each problem type or dimension
                 for tasks in PROBLEM_TYPES.keys():
                     for instance_amount in TEST_TRAINING_AMOUNT:
                         machines = PROBLEM_TYPES[tasks]
-                        # Si estoy generando instancias de entrenamiento, cambio el sufijo del path
+                        # If I'm generating training examples, the path suffix is changed
                         if instance_amount == TEST_TRAINING_AMOUNT[0]:
                             dir_suffix = '/test/'
                         else:
                             dir_suffix = '/training/'
-                        # sub_dir es el identificador de cada instancia del problema
+                        # sub_dir is the identifier for each problem instance
                         sub_dir = str(tasks) + 'x' + str(machines) + '-' + \
                             str(machine_amount) + str(task_heterogeneity_type) + \
                             str(machine_heterogeneity_type) + dir_suffix
-                        directory = GENERATOR_DIR + sub_dir
-                        # Agrego comandos al objeto commands
-                        commands.append(GENERATOR + str(tasks) + ' ' + str(machines) + ' ' \
-							+ str(machine_amount) + ' ' + str(task_heterogeneity_type) + ' ' \
-							+ str(machine_heterogeneity_type) + ' ' + str(instance_amount) \
-							+ ' ' + directory)
-                        # Genero directorio de salida que va a ser usado posteriormente
-                        generate_dir(directory)
+                        # If not generating raw data in a serial way, then the generation has to
+                        # be split up in jobs, so each job will generate raw data, instead of it
+                        # being generated in a serial single job
+                        # TODO polish this
+                        if not SERIAL_INITIAL_DATA_GENERATION:
+                            directory = GENERATOR_DIR + sub_dir
+                            # Add commands to commands object
+                            commands.append(GENERATOR + str(tasks) + ' ' + str(machines) + ' ' \
+                                + str(machine_amount) + ' ' + str(task_heterogeneity_type) + ' ' \
+                                + str(machine_heterogeneity_type) + ' ' + str(instance_amount) \
+                                + ' ' + directory)
+                            # Output directory is generated for later use
+                            generate_dir(directory)                            
                         directory = PARSER_DIR + sub_dir
                         commands.append(PARSER + str(instance_amount) + ' ' + PARENT_DIR \
-							+ GENERATOR_DIR + sub_dir + ' ' + PARENT_DIR + PARSER_DIR + sub_dir)
-                        # Genero directorio de salida que va a ser usado posteriormente
+                            + GENERATOR_DIR + sub_dir + ' ' + PARENT_DIR + PARSER_DIR + sub_dir)
+                        # Output directory is generated for later use
                         generate_dir(directory)
-    # Para cada job, agrego todos los comandos que correspondan generando un .sh
-    # Todo queda en el directorio ./jobs/
+    # For each job, every one of its commands is added in a single .sh file
+    # Everything will be stored at ./jobs/
     generate_dir(OUTPUT_PATH)
-    for index, job in enumerate(chunks(commands, JOB_AMOUNT)):
+    # Adjust command amount per job to actually generate JOB_AMOUNT jobs
+    commands_per_job = int(math.ceil(len(commands) / float(JOB_AMOUNT)))
+    for index, job in enumerate(chunks(commands, commands_per_job)):
         tmp_file = open(OUTPUT_PATH + 'job-' + str(index) + '.sh', 'w')
         for command in job:
             tmp_file.write(command + '\n')
         tmp_file.close()
-    print 'Scripts .sh creados en el directorio ' + OUTPUT_PATH
+    print 'Job scripts created at ' + OUTPUT_PATH        
 
 if __name__ == '__main__':
     main()
