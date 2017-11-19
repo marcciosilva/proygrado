d6488b69e0dc4c31a02c98c9ff9a54983065fc8d
Marccio Silva <msilva@tavanoteam.com>
2017-11-19 12:42:41 -0300

    Arreglos en generador de reportes (modelos no eran borrados tras primera instancia de creacion).

--- generador-instancias/generate-reports.sh
+++ generador-instancias/generate-reports.sh
@@ -5,7 +5,7 @@ echo $HEADER_MSG >> 'report_full_etc_sv'$FILE_EXT
 echo $HEADER_MSG >> 'report_partial_etc_svm'$FILE_EXT
 echo $HEADER_MSG >> 'report_full_etc_ann'$FILE_EXT
 echo $HEADER_MSG >> 'report_partial_etc_ann'$FILE_EXT
-for i in `seq 1 100`;
+for i in `seq 1 50`;
 do
     echo "-------------------------------------------"
     echo "-    Starting up iteration number "$i"       -"
@@ -15,7 +15,6 @@ do
     # Limpiar todo
     rm -rf data-raw/
     rm -rf data-processed/
-    rm -rf models/
     # Generar directorios y scripts de generacion de datos
     echo "#### Generating directories and jobs... ####"
     python generar_jobs.py
@@ -25,22 +24,14 @@ do
     # Procesar datos y convertirlos a .csv
     echo "#### Generating processed data... ####"
     sh jobs/job-0.sh
-    # Usando toda la ETC con SVM
-    echo "#### Generating report for full etc with SVM... ####"
+    # Usando toda la ETC
+    rm -rf models/
+    echo "#### Generating report for full etc with SVM and ANN... ####"
     current_file='report_full_etc_sv'
-    python classifier_generator.py 128 4 0 0 0 True 1 >> $current_file$FILE_EXT
-    # Usando una porcion de la ETC con SVM
-    echo "#### Generating report for partial etc with SVM... ####"
-    current_file='report_partial_etc_svm'
-    python classifier_generator.py 128 4 0 0 0 False 1 >> $current_file$FILE_EXT
-    # Usando toda la ETC con ANN
-    echo "#### Generating report for full etc with ANN... ####"
-    current_file='report_full_etc_ann'
-    python classifier_generator.py 128 4 0 0 0 True 0 >> $current_file$FILE_EXT
-    # Usando una porcion de la ETC con ANN
-    echo "#### Generating report for partial etc with ANN... ####"
-    current_file='report_partial_etc_ann'
-    python classifier_generator.py 128 4 0 0 0 False 0 >> $current_file$FILE_EXT
+    echo "#### Generating reports with full ETC... ####"
+    ((python classifier_generator.py 128 4 0 0 0 True 1 >> 'report_full_etc_svm'$FILE_EXT) & (python classifier_generator.py 128 4 0 0 0 True 0 >> 'report_full_etc_ann'$FILE_EXT)) \
+        && echo "#### Removing models to test with partial ETC... ####" && rm -rf models/ \
+        && ((python classifier_generator.py 128 4 0 0 0 False 1 >> 'report_partial_etc_svm'$FILE_EXT) & (python classifier_generator.py 128 4 0 0 0 False 0 >> 'report_partial_etc_ann'$FILE_EXT))
     now=`date +%s`
     runtime=$((now-start))
     echo "Runtime up until now is "$runtime" seconds"
