aa630eec7f0de106d4d6291e678251f7f99d6ddc
Mauro Picó <mauro@enia.io>
2017-12-11 23:07:19 -0300

    modificaciones en training para entrenar en ec2, nada que preocuparte

--- training/analisis.py
+++ training/analisis.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 
 USE_SVM = True
 USE_ANN = True
-CSV_QTY = 150
+CSV_QTY = 5
 task_amount = 3
 no = 1  # Amount of output neurons
 hidden_layer_amount = 2
@@ -24,16 +24,12 @@ def createClasiffier():
     pipa = Pipeline([ ('StandardScaler', StandardScaler()), ('classify',classifier) ])
     return pipa
 
-
 data = []
 for i in range(0,CSV_QTY):
     data.append(pandas.read_csv(str(i)+".csv", header=None, delimiter=','))
-    print("marccio puto")
 
-print("marccio puto2")
 training_testing = pandas.concat(data, ignore_index=True)
 training_testing.to_csv("variable.csv",sep=',',index=False,header=False)
-print("marccio puto2")
 
 if split_data_for_manual_tests:
     train, test = train_test_split(training_testing, train_size = 0.8)
@@ -41,15 +37,15 @@ if split_data_for_manual_tests:
     test_y = test.iloc[:,-1]
     del train.iloc[:,-1]
     del test.iloc[:,-1]
-
 else:
-    print("marccio puto3")
     target = training_testing.iloc[:,-1]
     del training_testing[len(training_testing.columns) -1]
-
     pipa = createClasiffier()
-    print("marccio puto4")
     scores = cross_val_score(pipa, training_testing, target, cv=5)
-    print(pipa.get_params())
-    print("marccio puto5")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    f = open('results.out', 'a')
+    f.write('\n\n' + "###################################################" + '\n')
+    f.write("###################################################" + '\n\n')
+    f.write(str(pipa.get_params()) + '\n')
+    f.write("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2) + '\n')
+    f.close()
