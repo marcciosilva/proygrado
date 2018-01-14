1948a7d47bfa5abb9a69a69be2b5a6ed9f5c0401
Marccio Silva <msilva@tavanoteam.com>
2018-01-14 14:18:11 -0300

    Refactor on classification module.

--- classification/problem_instance_classifier.py
+++ classification/problem_instance_classifier.py
@@ -1,29 +1,31 @@
-# receive problem instance somehow
-# levantar tantos clasificadores como sea
-# generar solucion
-# devolver solucion
-from sklearn.externals import joblib
 import pandas
-
-
-def load_csv_problem_instance():
-    return pandas.read_csv('problem_instance.csv', header=None, delimiter=',')
-
-def remove_target_column_from_dataframe(training_testing):
-    target = training_testing.iloc[:, -1]
-    del training_testing[len(training_testing.columns) - 1]
-    return target
-
+from sklearn.externals import joblib
 
 
 def main():
-    problem_instance_dataframe = load_csv_problem_instance()  # se pueden cargar instancias del problema con cualquier cantidad
-    # de tareas, pero el numero de maquinas es fijo
-    # o sea que si en la matriz tengo fila = tarea, columna = maquina, puedo tener tantas filas como quiera, pero cada fila
-    # sigue teniendo la misma cantidad de columnas (numero de maquinas)
-    target_column = remove_target_column_from_dataframe(problem_instance_dataframe)
-    classifier = joblib.load('classifier.pkl')
-    results = classifier.predict(problem_instance_dataframe)
+    results = classify_problem_instance('problem_instance.csv',
+                                        'classifier.pkl')  # TODO receive paths from calling module
     print(results)
 
-main()
+
+def classify_problem_instance(problem_instance_path, classifier_path):
+    problem_instance_dataframe = load_csv_problem_instance(problem_instance_path)
+    remove_target_column_from_dataframe(problem_instance_dataframe)
+    classifier = joblib.load(classifier_path)
+    results = classifier.predict(problem_instance_dataframe)  # TODO parallelize?
+    return results
+
+
+def load_csv_problem_instance(path):
+    return pandas.read_csv(path, header=None, delimiter=',')
+
+
+def remove_target_column_from_dataframe(dataframe):
+    target = dataframe.iloc[:, -1]
+    del dataframe[len(dataframe.columns) - 1]
+    return target
+
+
+# Only run main() if being called directly.
+if __name__ == '__main__':
+    main()
